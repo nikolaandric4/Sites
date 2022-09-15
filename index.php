3f3b745b6001df693fb3e9f9f0f53cd56c397aa8
nikolaandric4 <60518249+nikolaandric4@users.noreply.github.com>
2022-09-15 11:34:53 +0200

    Add files via upload

--- index.php
+++ index.php
@@ -1,69 +1,112 @@
+<div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="true">
 
-<div class="pricing-header p-3 pb-md-4 mx-auto text-center" style="background-color: goldenrod;">
-      <h1 class="display-4 fw-normal"><b>APARTMANI I SOBE</b></h1>
-      <p class="fs-5 text-muted">Hotel „ANDRIC“ je od 1967. godine, kada je završena petogodišnja izgradnja, do danas izrastao u pravi dragulj grada Prokuplja. Kompletno renovirana, danas ova građevina sija punim sjajem, prilagođena je najvišim svetskim standardima i čini turističku i privrednu krunu gradskog jezgra.</p>
+    <div class="carousel-indicators">
+
+
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+        <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
     </div>
 
-<div class="container">
-    
-<hr class="featurette-divider">
+    <div class="carousel-inner">
+        <div class="carousel-item active">
+            <img src="images/cover-nova-1.jpg" class="d-block w-100" alt="restoran" height="1000">
+        </div>
+        <div class="carousel-item">
+            <img src="images/lifedesign-hotel-belgrade-premium-suite-bedroom-1.png.1024x0.png" class="d-block w-100" alt="restoran" height="1000">
+        </div>
+        <div class="carousel-item">
+            <img src="images/Restoran.jpg" class="d-block w-100" alt="restoran" height="1000">
+        </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+        <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Previous</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+        <span class="carousel-control-next-icon" aria-hidden="true"></span>
+        <span class="visually-hidden">Next</span>
+    </button>
 
-    <div class="row featurette">
-      <div class="col-md-6">
-          <h2><span style="color: #333333;"><strong><em>Economy sobe</em></strong></span></h2>
-        <p style="text-align: center"><span style="color: #333333;">Sobe „economy“, kojih u hotelu „Hammeum“ ima koliko i apartmana – 6, veličine su 16,5 kvadratnih metara. Ne poseduju terasu, ali imaju&nbsp; prirodno osvetljenje. U njima se nalaze: radni sto, noćni stočić, mini-bar, sef, telefon i LCD televizor. Imaju besplatan Wi-Fi internet i opremljene su modernim klima uređajima i grejanjem.&nbsp;</span></p> <br>
-            
+</div>
+
+
+
+<div class=" container mt-4">
+    <h1><span style="color: #333333; margin-left: 500"><strong><em>Dobro dosli u Andric Hotel</em></strong></span></h1>
+
+    <div class="row mt-4" style="text-align: center;">
+        <div class="col-sm-3" style=""><img src="images/dobro-dosli-u-hotel-andric-678x1024.jpg" class="img-thumbnail" style="margin-left: 100;"></div>
+
+        <div class="col-sm-8">
+            <p>Turistički biser Prokuplja <br>
+                Hammeum hotel izgrađen je 1967. godine i dodao je posebnu notu gradu<br>
+                Prokuplju.Hotel je potpuno renoviran 2019. godine, a prilagođen je najvišim<br>
+                svetskim standardima. Smešten u srcu grada Prokuplja, hotel raspolaže sa 36<br>
+                smeštajnih jedinica, odnosno 6 economy, 21 standard, 3 standard superior<br>
+                sobe i 6 apartmana koji se prostiru na tri sprata. Spoj modernog i<br>
+                tradicionalnog ostavlja utisak topline, a svaka soba dizajnirana je tako da Vam<br>
+                pruži utisak kao da ste u svom domu.Sobe imaju prirodno osvetljenje, a u svakoj<br>
+                se nalazi radni sto, noćni stočić, mini-bar, sef, telefon i LCD televizor. Svaka soba<br>
+                izrađena je od najkvalitetnijih materijala, najfinije obrade, ima besplatan pristup<br>
+                Wi-Fi internetu i opremljena je modernim klima uređajima i grejanjem.<br>
+                Hammeum hotel sadrži luksuzno opremljen restoran „Festina Lente“, caffe<br>
+                poslastičarnica i picerija u kojoj se pizza pravi u peći na drva. Hotel poseduje i<br>
+                savremeno opremljenu konferencijsku salu do 35  mesta za sedenje, dečji<br>
+                kutak, parking prostor, ostavu za prtljag. Ljubazno i profesionalno osoblje hotela<br>
+                će se pobrinuti da doživite uslugu 5 zvezdica i da Vam svaki trenutak koji<br>
+                provedete u hotelu ostane u lepom sećanju.<br>
+                <br>
+                Vaše je samo da rezervišete sobu i uživate!<br>
+                <br>
+                Čekamo Vas… </p>
+
+        </div>
+    </div>
+</div>
+<div class="card-group mt-5">
+    <div class="card">
+        <img class="card-img-top" src="images/Apartman-1-1.jpg" alt="Card image cap">
+
+        <div class="card-body" style="text-align: center;">
+            <h5 class="card-title">APARTMANI</h5>
             <button type="button" class="w-100 btn btn-lg btn-outline-warning">Rezervacija</button>
-      </div>
-      <div class="col-md-5">
-          <img src="images/1 - Copy.jpg" alt="economy"/>
-      </div>
+        </div>
     </div>
-    <hr class="featurette-divider">
+    <div class="card">
+        <img class="card-img-top" src="images/Restoran.jpg" alt="Card image cap">
 
-    <div class="row featurette">
-      <div class="col-md-6">
-          <h2><span style="color: #333333;"><strong><em>Standard sobe</em></strong></span></h2>
-        <p style="text-align: center"><span style="color: #333333;">Sve standard sobe, kojih na tri sprata hotela „Hammeum“ ima 21 poseduju terasu, tako da s koje god strane hotela da ste smešteni, možete uživati u pogledu na gradsko šetalište ili predivno zelenilo gradskog parka pored Vas. Veličine su 22,2 kvadratna metra, a u njima se nalaze: radni sto, noćni stočić, mini-bar, sef, telefon i LCD televizor. Imaju besplatan Wi-Fi internet i opremljene su modernim klima uređajima i grejanjem.</span></p> <br>
-            
+        <div class="card-body" style="text-align: center;">
+            <h5 class="card-title">Restoran i Bar</h5>
             <button type="button" class="w-100 btn btn-lg btn-outline-warning">Rezervacija</button>
-      </div>
-      <div class="col-md-5">
-          <img src="images/2 - Copy.jpg" alt="economy"/>
-      </div>
+        </div>
     </div>
-    <hr class="featurette-divider">
+    <div class="card">
+        <img class="card-img-top" src="images/hotel-hammeum-slider-010jpg-768x512 - Copy.jpg" alt="Card image cap">
 
-   <div class="row featurette">
-      <div class="col-md-6">
-          <h2><span style="color: #333333;"><strong><em>Standard superior sobe</em></strong></span></h2>
-        <p style="text-align: center"><span style="color: #333333;">Sobe koje spadaju u kategoriju „standard superior“, a kojih u hotelu „Hammeum“ ima tri, veličine su 16,5 kvadratnih metara, ali imaju veće terase i kupatila od standard soba. U njima se, takođe, nalaze: radni sto, noćni stočić, mini-bar, sef, telefon i LCD televizor. Superior standard sobe Imaju besplatan Wi-Fi internet i opremljene su modernim klima uređajima i grejanjem.</span></p> <br>
-            
-        <button type="button" class="w-100 btn btn-lg btn-outline-warning">Rezervacija</button>
-      </div>
-      <div class="col-md-5">
-          <img src="images/343158290 - Copy_1.jpg" alt="economy"/>
-
-      </div>
-    </div>
-    
-
-    <hr class="featurette-divider">
-    <div class="row featurette">
-      <div class="col-md-6">
-          <h2><span style="color: #333333;"><strong><em>Apartmani</em></strong></span></h2>
-        <p style="text-align: center"><span style="color: #333333;">Hotel „Hammeum“ poseduje 6 apartmana na tri sprata. Njihova veličina je od 26 do 39 kvadratnih metara, a tri od šest aprtmana su sa terasom.  U aprtmanima se, kao i u svim sobama hotela nalaze: radni sto, noćni stočić, mini-bar, sef, telefon i LCD televizor. Svaki apartman ima besplatan Wi-Fi internet i opremljen je modernim klima uređajima i grejanjem.
-
-Osim dva kreveta, koja se mogu spojiti po želji gosta, apartmani imaju francuski ležaj i sofu u dnevnoj sobi, koja se može namestiti da bude za dvoje, tako da apartman može primiti do četvoro ljudi</span></p> <br>
-            
+        <div class="card-body" style="text-align: center">
+            <h5 class="card-title">Konferencijska sala</h5>
             <button type="button" class="w-100 btn btn-lg btn-outline-warning">Rezervacija</button>
-      </div>
-      <div class="col-md-5">
-          <img src="images/4 - Copy.jpg" alt="economy"/>
-
-      </div>
+        </div>
     </div>
-    
-        <hr class="featurette-divider">
+</div>
 
+<div class="container marketing">
+
+    <!-- Three columns of text below the carousel -->
+    <div class="row mt-4" style="text-align: right">
+        <div class="col-lg-4">
+            <img width="200" height="200" src="images/wifi@3x.png" class="attachment-large size-large" alt="" loading="lazy">
+            <h4>Besplatan Wi-Fi&nbsp;</h4>
+            <p><span style="color: #333333;">Svim posetiocima hotela obezbeđena je brza internet veza.</span></p>
+
+        </div><!-- /.col-lg-4 -->
+        <div class="col-lg-4">
+            <img width="200" height="200" src="images/parking@3x.png" class="attachment-large size-large" alt="" loading="lazy">
+            <h4>Besplatan parking&nbsp;</h4>
+            <p><span style="color: #333333;">Svim gostima hotela obezbeđen je parking.</span></p>
+
+        </div>
     </div>
+</div>   
+
